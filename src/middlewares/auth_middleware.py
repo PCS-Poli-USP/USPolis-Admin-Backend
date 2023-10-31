4294fef3db5452186c6638b72a7b2ea6fa8ad6d2
Henrique Duran <henriqueduran15@gmail.com>
2023-10-31 14:39:39 -0300

    :lock: boto config on .env

--- src/middlewares/auth_middleware.py
+++ src/middlewares/auth_middleware.py
@@ -1,21 +1,25 @@
 from flask import request
 import boto3
+import os
 
 
 def auth_middleware():
-    client = boto3.client("cognito-idp")
+    client = boto3.client(
+        "cognito-idp",
+        region_name=os.environ.get("AWS_REGION"),
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    )
     try:
         authorization = request.headers.get("Authorization")
         token = authorization.split(" ")[1]
         if token is None:
             return {"message": "Missing authorization header"}, 400
 
-        response = client.get_user(
-            AccessToken=token
-        )
+        response = client.get_user(AccessToken=token)
 
         request.user = response
-    
+
     except Exception as e:
         return {"message": "Invalid token"}, 400
 
--- src/services/user/user_services.py
+++ src/services/user/user_services.py
@@ -6,7 +6,12 @@ UserExistsException = type("CustomUserExistsException", (Exception,), {})
 
 def cognito_create_user(username: str, email: str):
     user_pool_id = os.environ.get("USER_POOL_ID")
-    client = boto3.client("cognito-idp")
+    client = boto3.client(
+        "cognito-idp",
+        region_name=os.environ.get("AWS_REGION"),
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    )
     try:
         response = client.admin_create_user(
             UserPoolId=user_pool_id,
