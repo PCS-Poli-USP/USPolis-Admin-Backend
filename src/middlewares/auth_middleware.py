4ff38844c86d178a15e33e22c152fe62ffc5d37c
Henrique Duran <henriqueduran15@gmail.com>
2023-10-31 21:36:28 -0300

    ♻️ admin middleware: refactored to use less code

--- src/middlewares/auth_middleware.py
+++ src/middlewares/auth_middleware.py
@@ -31,28 +31,10 @@ def auth_middleware():
 def admin_middleware():
     user_repository = UserRepository()
 
-    client = boto3.client(
-        "cognito-idp",
-        region_name=os.environ.get("AWS_REGION"),
-        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
-        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
-    )
-
-    try:
-        authorization = request.headers.get("Authorization")
-        token = authorization.split(" ")[1]
-        if token is None:
-            return {"message": "Missing authorization header"}, 400
-
-        response = client.get_user(AccessToken=token)
-
-        request.user = response
-
-    except Exception as e:
-        return {"message": "Invalid token"}, 400
-
-    finally:
-        client.close()
+    auth_middleware_response = auth_middleware()
+    
+    if auth_middleware_response is not None:
+        return auth_middleware_response
 
     if not user_repository.is_admin(request.user["Username"]):
         return {"message": "Unauthorized"}, 401
