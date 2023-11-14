004c04b2233b467628a20956d7ee6e8d21d870cf
FastShowDev <zlfastshow@hotmail.com>
2023-11-14 10:38:30 -0300

    Fix auth middleware error

--- src/middlewares/auth_middleware.py
+++ src/middlewares/auth_middleware.py
@@ -5,16 +5,17 @@ client = boto3.client("cognito-idp")
 
 def auth_middleware():
     try:
-        authorization = request.headers.get("Authorization")
-        token = authorization.split(" ")[1]
-        if token is None:
-            return {"message": "Missing authorization header"}, 400
+        if request.method != 'OPTIONS':
+            authorization = request.headers.get("Authorization")
+            token = authorization.split(" ")[1]
+            if token is None:
+                return {"message": "Missing authorization header"}, 400
 
-        response = client.get_user(
-            AccessToken=token
-        )
+            response = client.get_user(
+                AccessToken=token
+            )
 
-        request.user = response
+            request.user = response
     
     except Exception as e:
         return {"message": "Invalid token"}, 400
