a4732e10f1628dedf266cbe509a808e907024af9
Henrique Duran <henriqueduran15@gmail.com>
2024-01-16 07:48:21 -0300

    :hammer: FIX: database default value

--- src/common/database.py
+++ src/common/database.py
@@ -1,11 +1,12 @@
 import os
-from pymongo import MongoClient
+
 from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
 
 load_dotenv()
 
 uri = os.environ.get("CONN_STR")
-db_name = os.environ.get("DB_NAME")
+db_name = os.environ.get("DB_NAME") or "uspolis"
 mongo_client = MongoClient(uri, 27017)
 
 # check databases
