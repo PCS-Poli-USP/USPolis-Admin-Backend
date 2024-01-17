1f2eec89fac345cbf0477b4decc7a92ce7c1d997
FastShowDev <zlfastshow@hotmail.com>
2024-01-16 21:15:38 -0300

    Create reverse days indexer

--- src/common/utils/days.py
+++ src/common/utils/days.py
@@ -16,3 +16,22 @@ def days_index(day: str):
       return 7 
     case _:
       return 8
+    
+def reverse_days_index(day: str):
+  match day:
+    case "seg":
+      return 7
+    case "ter":
+      return 6
+    case "qua":
+      return 5
+    case "qui":
+      return 4
+    case "sex":
+      return 3
+    case "sab":
+      return 2
+    case "dom":
+      return 1 
+    case _:
+      return 0
