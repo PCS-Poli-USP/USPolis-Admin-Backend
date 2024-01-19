1357a82b77751740427a2ea319bb04268a1fed87
Henrique Duran <henriqueduran15@gmail.com>
2024-01-19 14:21:38 -0300

    FIX: prettified conflicts ids

--- src/blueprints/conflict_blueprint.py
+++ src/blueprints/conflict_blueprint.py
@@ -2,6 +2,7 @@ from bson.json_util import dumps
 from flask import Blueprint, request
 
 from src.blueprints.blueprint_builder import build_authenticated_blueprint
+from src.common.utils.prettify_id import recursive_prettify_id
 from src.repository.conflicts_repository import ConflictRepository
 
 conflict_blueprint = build_authenticated_blueprint("conflicts", "/api/conflicts")
@@ -11,8 +12,9 @@ conflict_blueprint = build_authenticated_blueprint("conflicts", "/api/conflicts"
 def get_conflicts():
     conflictRepository = ConflictRepository()
     try:
-        confclits = conflictRepository.get_all()
-        return dumps(confclits), 200
+        conflicts = conflictRepository.get_all()
+        recursive_prettify_id(conflicts)
+        return dumps(conflicts), 200
 
     # TODO: handle exceptions
     except Exception as e:
