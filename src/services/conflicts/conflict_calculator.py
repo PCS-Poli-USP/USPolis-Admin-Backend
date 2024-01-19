04a05f4981586d7714d94336233d9613964228c4
Henrique Duran <henriqueduran15@gmail.com>
2024-01-19 12:26:24 -0300

    FIX: grouped the conflict response

--- src/services/conflicts/conflict_calculator.py
+++ src/services/conflicts/conflict_calculator.py
@@ -3,8 +3,6 @@ from itertools import groupby
 
 
 class ConflictCalculator:
-    # TODO: group the response by buildings, classrooms and week_days
-
     def __init__(self, events: list[dict]):
         self.__all_events = events
 
@@ -23,35 +21,6 @@ class ConflictCalculator:
         grouped_result = self.__group_conflicts()
         return grouped_result
 
-    def __group_conflicts(self) -> dict:
-        result = self.__group_events_by_key(self.__conflicts, "building")
-        for building in result:
-            result[building] = self.__group_events_by_key(result[building], "classroom")
-            for classroom in result[building]:
-                # TODO: special treatment for sorting week_days,
-                # its not alfabetical order
-                result[building][classroom] = self.__group_events_by_key(
-                    result[building][classroom], "week_day"
-                )
-                for week_day in result[building][classroom]:
-                    result[building][classroom][week_day].sort(
-                        key=lambda x: self.__parse_time(x["start_time"])
-                    )
-        return result
-
-    def __group_events_by_key(self, events: list[dict], key: str) -> dict:
-        events.sort(key=lambda x: x[key])
-        return {
-            key: list(group) for key, group in groupby(events, key=lambda x: x[key])
-        }
-
-    def __add_conflict(self):
-        if self.__event1 not in self.__conflicts:
-            self.__conflicts.append(self.__event1)
-
-        if self.__event2 not in self.__conflicts:
-            self.__conflicts.append(self.__event2)
-
     def __check_conflict(self) -> bool:
         if (
             self.__check_field_equal("building")
@@ -60,6 +29,13 @@ class ConflictCalculator:
         ):
             return self.__check_time_overlap()
         return False
+
+    def __add_conflict(self):
+        if self.__event1 not in self.__conflicts:
+            self.__conflicts.append(self.__event1)
+
+        if self.__event2 not in self.__conflicts:
+            self.__conflicts.append(self.__event2)
 
     def __check_time_overlap(self) -> bool:
         start_time1 = self.__parse_time(self.__event1.get("start_time"))
@@ -74,8 +50,37 @@ class ConflictCalculator:
             or (start_time2 <= start_time1 and end_time2 >= end_time1)
         )
 
+    def __group_conflicts(self) -> dict:
+        self.__conflicts.sort(key=lambda x: x["building"])
+        grouped_building_data = self.__group_events_by_key(self.__conflicts, "building")
+        result = {}
+        buildings_list = []
+        for building, building_data in grouped_building_data.items():
+            result = {"buildings": []}
+            grouped_classroom_data = self.__group_events_by_key(
+                building_data, "classroom"
+            )
+            classrooms_list = []
+            for classroom, classroom_data in grouped_classroom_data.items():
+                grouped_week_day_data = self.__group_events_by_key(
+                    classroom_data, "week_day"
+                )
+                week_days_list = []
+                for week_day, week_day_data in grouped_week_day_data.items():
+                    week_days_list.append({"name": week_day, "events": week_day_data})
+                classrooms_list.append({"name": classroom, "week_days": week_days_list})
+            buildings_list.append({"name": building, "classrooms": classrooms_list})
+        result = {"buildings": buildings_list}
+        return result
+
     def __parse_time(self, time):
         return datetime.strptime(time, "%H:%M")
 
     def __check_field_equal(self, field: str) -> bool:
         return self.__event1.get(field) == self.__event2.get(field)
+
+    def __group_events_by_key(self, events: list[dict], key: str) -> dict:
+        events.sort(key=lambda x: x[key])
+        return {
+            key: list(group) for key, group in groupby(events, key=lambda x: x[key])
+        }
