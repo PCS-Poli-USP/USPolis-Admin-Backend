dfa4454b00204c889ef6064a23fa5f5cb0982e43
FastShowDev <zlfastshow@hotmail.com>
2024-02-27 19:42:19 -0300

    Change cache timeout

--- src/common/cache.py
+++ src/common/cache.py
@@ -3,7 +3,7 @@ from flask_caching import Cache
 config = {
     "DEBUG": True,
     "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 60 * 60 # 1 hour
+    "CACHE_DEFAULT_TIMEOUT": 60 * 5 # 1 hour
 }
 
 cache = Cache(config=config)
