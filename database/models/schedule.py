86eb7d213182f5fe588b4e7698f38a40a88dfd67
Henrique Duran <henriqueduran15@gmail.com>
2024-04-30 14:19:34 -0300

    some schemas

---
+++ database/models/schedule.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from beanie import Document, Link
+from pydantic import BaseModel
+
+from database.models.university_class import Class
+from database.models.subject import Subject
+
+class WeekDay(Enum):
+    MONDAY = "Monday"
+    TUESDAY = "Tuesday"
+
+class Schedule(Document):
+    university_class: Link[Class]
+    week_day: WeekDay
+    start_date: datetime
+    
---
+++ database/models/university_class.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from typing import List, Optional
+from beanie import Document, Link
+from pydantic import BaseModel
+
+from database.models.subject import Subject
+
+class Preferences(BaseModel):
+    air_conditionating: bool
+    accessibility: bool
+    projector: bool
+
+class Class(Document):
+    subject: Link[Subject]
+    period: List[str]
+    start_period: datetime
+    end_period: datetime
+    class_type: str
+    vacancies: int
+    subscribers: int
+    pendings: int
+    preferences: Preferences
+    ignore_to_allocate: Optional[bool]
+    full_allocated: Optional[bool]
+    updated_at: datetime
+    # creted_by: Link[User]
+    # schedule: Link[Schedule]
