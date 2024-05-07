c7b9ffaf51023f18e7267b6b8b5d338e9ef2e794
Henrique Duran <henriqueduran15@gmail.com>
2024-05-07 16:49:52 -0300

    parametrized crawler test

--- tests/services/jupiter_crawler/test_crawler.py
+++ tests/services/jupiter_crawler/test_crawler.py
@@ -7,18 +7,23 @@ from tests.services.jupiter_crawler.utils import JupiterCrawlerTestUtils
 
 
 @pytest.mark.asyncio
-async def test_crawler() -> None:
-    subject_codes_to_test = JupiterCrawlerTestUtils.retrieve_subject_codes_to_test()
-    reference_results = JupiterCrawlerTestUtils.retrieve_reference_results()
+async def test_crawler(subject_code: str) -> None:
+    reference_result = JupiterCrawlerTestUtils.retrieve_reference_results()[subject_code]
     page_contents = JupiterCrawlerTestUtils.retrieve_html_contents()
+    result = await JupiterCrawler.crawl_subject_static(
+        subject_code, page_contents[subject_code]
+    )
 
-    for subject_code in subject_codes_to_test:
-        reference_result = reference_results[subject_code]
-        result = await JupiterCrawler.crawl_subject_static(
-            subject_code, page_contents[subject_code]
-        )
+    result_json: dict = json.loads(result.model_dump_json())
+    reference_result_json: dict = json.loads(reference_result.model_dump_json())
 
-        result_json: dict = json.loads(result.model_dump_json())
-        reference_result_json: dict = json.loads(reference_result.model_dump_json())
+    assert result_json == reference_result_json
 
-        assert result_json == reference_result_json
+# Define your test cases as a list of subject codes
+test_cases = JupiterCrawlerTestUtils.retrieve_subject_codes_to_test()
+
+# Parametrize the test function with the list of test cases
+@pytest.mark.parametrize("subject_code", test_cases)
+def test_crawler_parametrized(subject_code: str) -> None:
+    pytest.mark.asyncio(test_crawler(subject_code))
+
