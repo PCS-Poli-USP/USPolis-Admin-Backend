722d85f8fe243d5d3e102fe173767e25d791c447
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-05-07 15:02:34 -0300

    Add default admin user after each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,10 +9,12 @@ from fastapi import FastAPI
 from httpx import AsyncClient
 
 from server.config import CONFIG
+from tests.utils.user_test_utils import add_admin_user
 
 # Override config settings before loading the app
 CONFIG.testing = True
-CONFIG.mongo_uri = config("TEST_MONGO_URI", default="mongodb://localhost:27017")
+CONFIG.mongo_uri = config(
+    "TEST_MONGO_URI", default="mongodb://localhost:27017")
 CONFIG.mongo_db_name = config("TEST_MONGO_DB_NAME", default="uspolis-test")
 
 from server import app  # noqa: E402
@@ -21,7 +23,8 @@ from server import app  # noqa: E402
 async def clear_database(server: FastAPI) -> None:
     """Empty the test database."""
     async for collection in await server.db.list_collections():  # type: ignore[attr-defined]
-        await server.db[collection["name"]].delete_many({})  # type: ignore[attr-defined]
+        # type: ignore[attr-defined]
+        await server.db[collection["name"]].delete_many({})
 
 
 @pytest_asyncio.fixture()
@@ -35,3 +38,4 @@ async def client() -> AsyncIterator[AsyncClient]:
                 print(exc)
             finally:
                 await clear_database(app)
+                await add_admin_user()
