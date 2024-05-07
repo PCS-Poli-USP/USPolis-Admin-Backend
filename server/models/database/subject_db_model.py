75e069d9f2cbcacf7004718071c896d2821c2bdd
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-05-07 15:02:20 -0300

    Fix error in Subject by id

--- server/models/database/subject_db_model.py
+++ server/models/database/subject_db_model.py
@@ -24,17 +24,21 @@ class Subject(Document):
 
     @classmethod
     async def by_id(cls, id: str) -> Self:
-        subject = await Subject.by_id(id)
+        subject = await Subject.get(id)
         if subject is None:
             raise SubjectNotFound(id)
         return subject
 
     @classmethod
-    async def by_code(cls, code: str) -> Self | None:
+    async def by_code(cls, code: str) -> Self:
         subject = await Subject.find_one(cls.code == code)
         if subject is None:
             raise SubjectNotFound(code)
         return subject
+
+    @classmethod
+    async def check_code_exists(cls, code: str) -> bool:
+        return await cls.find_one(cls.code == code) is not None
 
 
 class SubjectNotFound(HTTPException):
