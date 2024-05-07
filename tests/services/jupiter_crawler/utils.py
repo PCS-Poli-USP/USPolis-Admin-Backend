21fdc51c772ac1136b173e4c24dceea44bee5db1
Henrique Duran <henriqueduran15@gmail.com>
2024-05-07 17:00:44 -0300

    test cases

--- tests/services/jupiter_crawler/utils.py
+++ tests/services/jupiter_crawler/utils.py
@@ -5,7 +5,7 @@ from server.services.jupiter_crawler.models import CrawledSubject
 
 
 class JupiterCrawlerTestUtils:
-    _test_cases_file_path = "tests/data/jupiter_crawler/crawler_test_cases.txt"
+    _test_cases_file_path = "tests/test_cases/crawler_test_cases.txt"
     _contents_file_path = "tests/data/jupiter_crawler/html_contents"
     _reference_results_file_path = "tests/data/jupiter_crawler/reference_results"
 
