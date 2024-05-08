c6b988acddc59aaf41e2e1478bf1b0fe00069444
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-05-08 08:25:35 -0300

    Update logic for update in Building, Subject, Classroom, Holiday Category

--- server/routes/subject_routes.py
+++ server/routes/subject_routes.py
@@ -20,7 +20,7 @@ async def get_all_subjects() -> list[Subject]:
 @router.get("/{subject_id}")
 async def get_subject(subject_id: str) -> Subject:
     """Get a subject"""
-    return await Subject.by_id(subject_id) # type: ignore
+    return await Subject.by_id(subject_id)  # type: ignore
 
 
 @router.post("")
@@ -46,6 +46,8 @@ async def create_subject(subject_input: SubjectRegister) -> str:
 @router.patch("/{subject_id}")
 async def update_subject(subject_id: str, subject_input: SubjectRegister) -> str:
     """Update a subject"""
+    if not await Subject.check_code_is_valid(subject_id, subject_input.code):
+        raise SubjectCodeAlreadyExists(subject_input.code)
     new_subject = await Subject.by_id(subject_id)
     await new_subject.update({"$set": subject_input})
     return str(new_subject.id)
@@ -55,9 +57,10 @@ async def update_subject(subject_id: str, subject_input: SubjectRegister) -> str
 async def delete_subject(subject_id: str) -> int:
     """Delete a subject"""
     subject = await Subject.by_id(subject_id)
-    response = await subject.delete() # type: ignore
+    response = await subject.delete()  # type: ignore
     if response is None:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "No subject deleted")
+        raise HTTPException(
+            status.HTTP_500_INTERNAL_SERVER_ERROR, "No subject deleted")
     return int(response.deleted_count)
 
 
