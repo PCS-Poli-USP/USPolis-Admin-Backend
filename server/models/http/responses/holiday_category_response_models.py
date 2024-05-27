2573b63aa396bbc2dc2df6775a81c2452488fee7
FastShowDev <zlFastShow@hotmail.com>
2024-05-27 15:38:49 -0300

    Update holidays response models

--- server/models/http/responses/holiday_category_response_models.py
+++ server/models/http/responses/holiday_category_response_models.py
@@ -4,19 +4,29 @@ from server.models.database.holiday_category_db_model import HolidayCategory
 
 
 class HolidayCategoryResponse(BaseModel):
-    id: str
+    id: int
     name: str
     created_by: str
 
     @classmethod
-    async def from_holiday_category(cls, holiday_category: HolidayCategory) -> "HolidayCategoryResponse":
-        await holiday_category.fetch_all_links()
+    def from_holiday_category(
+        cls, holiday_category: HolidayCategory
+    ) -> "HolidayCategoryResponse":
+        if holiday_category.id is None:
+            raise ValueError(
+                "HolidayCategory ID is None, try refreshing the session if it is newly created"
+            )
         return cls(
-            id=str(holiday_category.id),
+            id=holiday_category.id,
             name=holiday_category.name,
             created_by=holiday_category.created_by.name,  # type: ignore
         )
 
     @classmethod
-    async def from_holiday_category_list(cls, holidays_categories: list[HolidayCategory]) -> list["HolidayCategoryResponse"]:
-        return [await cls.from_holiday_category(holiday_category) for holiday_category in holidays_categories]
+    def from_holiday_category_list(
+        cls, holidays_categories: list[HolidayCategory]
+    ) -> list["HolidayCategoryResponse"]:
+        return [
+            cls.from_holiday_category(holiday_category)
+            for holiday_category in holidays_categories
+        ]
