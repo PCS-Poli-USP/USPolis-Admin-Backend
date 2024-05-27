c7753fe394f2a95ec8c01b435cc1c758aa0e73d2
FastShowDev <zlFastShow@hotmail.com>
2024-05-27 15:39:30 -0300

    Create holidays repositories

---
+++ server/repositories/holidays_categories_repository.py
@@ -0,0 +1,70 @@
+from fastapi import HTTPException, status
+from sqlmodel import Session, col, select
+
+from server.models.database.holiday_category_db_model import HolidayCategory
+from server.models.database.user_db_model import User
+from server.models.http.requests.holiday_category_request_models import (
+    HolidayCategoryRegister,
+    HolidayCategoryUpdate,
+)
+
+
+class HolidayCategoryRepository:
+    @staticmethod
+    def get_all(*, session: Session) -> list[HolidayCategory]:
+        statement = select(HolidayCategory)
+        holidays_categories = session.exec(statement).all()
+        return list(holidays_categories)
+
+    @staticmethod
+    def get_by_id(*, id: str, session: Session) -> HolidayCategory:
+        statement = select(HolidayCategory).where(col(HolidayCategory.id) == id)
+        holiday_category = session.exec(statement).one()
+        return holiday_category
+
+    @staticmethod
+    def get_by_name(*, name: str, session: Session) -> HolidayCategory:
+        statement = select(HolidayCategory).where(col(HolidayCategory.name) == name)
+        holiday_category = session.exec(statement).one()
+        return holiday_category
+
+    @staticmethod
+    def create(
+        *, creator: User, input: HolidayCategoryRegister, session: Session
+    ) -> HolidayCategory:
+        new_holiday_category = HolidayCategory(
+            name=input.name,
+            created_by=creator,
+        )
+        session.add(new_holiday_category)
+        session.commit()
+        session.refresh(new_holiday_category)
+        return new_holiday_category
+
+    @staticmethod
+    def update(
+        *, id: str, input: HolidayCategoryUpdate, user: User, session: Session
+    ) -> HolidayCategory:
+        holiday_category = HolidayCategoryRepository.get_by_id(id=id, session=session)
+        if holiday_category.created_by_id != user.id:
+            raise HolidayCategoryOperationNotAllowed("update", holiday_category.name)
+        holiday_category.name = input.name
+        session.add(holiday_category)
+        session.commit()
+        return holiday_category
+
+    @staticmethod
+    def delete(*, id: str, user: User, session: Session) -> None:
+        holiday_category = HolidayCategoryRepository.get_by_id(id=id, session=session)
+        if holiday_category.created_by_id != user.id:
+            raise HolidayCategoryOperationNotAllowed("delete", holiday_category.name)
+        session.delete(holiday_category)
+        session.commit()
+
+
+class HolidayCategoryOperationNotAllowed(HTTPException):
+    def __init__(self, operation: str, holiday_category_info: str) -> None:
+        super().__init__(
+            status.HTTP_401_UNAUTHORIZED,
+            f"Not Allowed to {operation} HolidayCategory {holiday_category_info}",
+        )
