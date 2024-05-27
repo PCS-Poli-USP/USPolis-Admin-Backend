3dd94722f29e2a183c23445cfca2fbde3282f921
FastShowDev <zlFastShow@hotmail.com>
2024-05-27 15:38:37 -0300

    Update holidays request models

--- server/models/http/requests/holiday_request_models.py
+++ server/models/http/requests/holiday_request_models.py
@@ -1,16 +1,12 @@
 from pydantic import BaseModel
 from datetime import datetime
 
-from server.utils.enums.holiday_type import HolidayType
-
 
 class HolidayRegister(BaseModel):
     category_id: str
     date: datetime
-    type: HolidayType
 
 
 class HolidayUpdate(BaseModel):
     category_id: str
     date: datetime
-    type: HolidayType
