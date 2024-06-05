d1ac4f6218451406ac740d2bc8f97735eac874bd
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-06-04 21:59:45 -0300

    Fix institutional event repository

--- server/repositories/institutional_event_repository.py
+++ server/repositories/institutional_event_repository.py
@@ -6,20 +6,12 @@ from server.models.database.institutional_event_db_model import InstitutionalEve
 from server.models.http.requests.institutional_event_request_models import (
     InstitutionalEventRegister,
 )
-from server.models.http.responses.institutional_event_response_models import (
-    InstitutionalEventResponse,
-)
-from server.models.http.requests.institutional_event_request_models import \
-    InstitutionalEventRegister
-from server.models.http.responses.institutional_event_response_models import \
-    InstitutionalEventResponse
 
 
 class InstitutionalEventRepository:
     @staticmethod
     def get_by_id(*, id: int, session: Session) -> InstitutionalEvent:
-        statement = select(InstitutionalEvent).where(
-            col(InstitutionalEvent.id) == id)
+        statement = select(InstitutionalEvent).where(col(InstitutionalEvent.id) == id)
         event = session.exec(statement).one()
         return event
 
@@ -30,7 +22,9 @@ class InstitutionalEventRepository:
         return list(events)
 
     @staticmethod
-    def create(*, input: InstitutionalEventRegister, session: Session) -> InstitutionalEventResponse:
+    def create(
+        *, input: InstitutionalEventRegister, session: Session
+    ) -> InstitutionalEvent:
         new_event = InstitutionalEvent(
             title=input.title,
             description=input.description,
@@ -46,4 +40,4 @@ class InstitutionalEventRepository:
         session.add(new_event)
         session.commit()
         session.refresh(new_event)
-        return InstitutionalEventResponse.from_institutional_event(new_event)
+        return new_event
