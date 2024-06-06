ae8887fbc0c5cc35c26075db007809afb4df9a33
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-06-06 06:29:11 -0300

    Migrate ocurrence db model to sqlmodel

--- server/models/database/occurence_db_model.py
+++ server/models/database/occurence_db_model.py
@@ -1,18 +1,26 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
-from beanie import Document, Link
+from sqlmodel import Field, Relationship, SQLModel
 
-from server.models.database.classroom_db_model import Classroom
-from server.models.database.schedule_db_model import Schedule
+if TYPE_CHECKING:
+    from server.models.database.classroom_db_model import Classroom
+    from server.models.database.schedule_db_model import Schedule
+
 from server.utils.day_time import DayTime
 
 
-class Ocurrence(Document):
-    classroom: Link[Classroom]
-    schedule: Link[Schedule]
-    start_time: DayTime
-    end_time: DayTime
-    date: datetime
+class Ocurrence(SQLModel, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    start_time: DayTime = Field()
+    end_time: DayTime = Field()
+    date: datetime = Field()
 
-    class Settings:
-        name = "occurrences"
+    classroom_id: int | None = Field(
+        default=None, foreign_key="classroom.id", nullable=False
+    )
+    classroom: "Classroom" = Relationship(back_populates="ocurrences")
+    schedule_id: int | None = Field(
+        default=None, index=True, foreign_key="schedule.id", nullable=False
+    )
+    schedule: "Schedule" = Relationship(back_populates="ocurrences")
