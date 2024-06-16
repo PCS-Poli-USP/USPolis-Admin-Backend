64750e0abe18ef6d3eceb813fbf04e557a571655
Henrique Duran <henriqueduran15@gmail.com>
2024-06-16 19:12:27 -0300

    test config to delete tables

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from collections.abc import Generator
 import pytest
 from decouple import config  # type: ignore [import-untyped]
 from fastapi.testclient import TestClient
+from sqlalchemy import text
 from sqlmodel import Session
 
 from server.config import CONFIG
@@ -15,25 +16,40 @@ CONFIG.override_cognito_client = True
 CONFIG.db_uri = config("TEST_DATABASE_URI")  # type: ignore
 CONFIG.db_database = config("TEST_DATABASE_NAME", default="uspolis-test")  # type: ignore
 
-# Override config settings before loading the app
-from server.app import app  # noqa: E402
+from server.app import app  # noqa
 from server.models.database.building_db_model import Building  # noqa  # noqa
 from server.models.database.user_db_model import User  # noqa
 from server.repositories.users_repository import UserRepository  # noqa
 from server.scripts.initial_data import init_db  # noqa
+from server.db import engine  # noqa
 
+# create function to delete all data from tables
+with Session(engine) as session:
+    statement = text("""
+    CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS $$
+    DECLARE
+        statements CURSOR FOR
+            SELECT tablename FROM pg_tables
+            WHERE tableowner = username AND schemaname = 'public';
+    BEGIN
+        FOR stmt IN statements LOOP
+            EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+        END LOOP;
+    END;
+    $$ LANGUAGE plpgsql;
+    """)
+    session.execute(statement)
+    session.commit()
 
 @pytest.fixture(scope="session", autouse=True)
 def db() -> Generator[Session, None, None]:
-    from server.db import engine  # noqa
 
     with Session(engine) as session:
         init_db(session)
         yield session
-        # session.execute(delete(UserBuildingLink))
-        # session.execute(delete(Building))
-        # session.execute(delete(User))
-        # session.commit()
+        statement = text("SELECT truncate_tables('postgres');")
+        session.execute(statement)
+        session.commit()
 
 
 @pytest.fixture(scope="module")
