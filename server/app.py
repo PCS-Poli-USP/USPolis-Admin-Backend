da5cbf6ede5b968f13ac82ed97fd3fcbd47f7443
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-06-30 23:17:00 -0300

    Rename routes folder

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from starlette.middleware.cors import CORSMiddleware
 from server.deps_overrides import DepsOverrides
 from server.exception_handlers import add_exception_handlers
 from server.routes.admin import router as AdminRouter
-from server.routes.public import router as PublicRouter
+from server.routes.restricted import router as PublicRouter
 
 app = FastAPI(
     title="USPolis Server",
--- tests/utils/classroom_test_utils.py
+++ tests/utils/classroom_test_utils.py
@@ -4,7 +4,7 @@ from server.models.database.building_db_model import Building
 from server.models.database.classroom_db_model import Classroom
 from server.models.database.user_db_model import User
 from server.models.http.requests.classroom_request_models import ClassroomRegister
-from server.routes.public.classroom_routes import ClassroomInBuildingAlredyExists
+from server.routes.restricted.classroom_routes import ClassroomInBuildingAlredyExists
 from tests.utils.default_values.test_classroom_default_values import (
     ClassroomDefaultValues,
 )
