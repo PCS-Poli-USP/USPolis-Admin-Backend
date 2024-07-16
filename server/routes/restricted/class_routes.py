eac3f80ce909d72390576b8735a6dde39d9f4086
Gabriel Di Vanna Camargo <zlFastShow@hotmail.com>
2024-07-16 12:55:09 -0300

    Create class repository adapter

--- server/routes/restricted/class_routes.py
+++ server/routes/restricted/class_routes.py
@@ -1,11 +1,12 @@
 from typing import Annotated
 from fastapi import APIRouter, Body, Query, Response
 
-from server.deps.session_dep import SessionDep
+from server.deps.repository_adapters.class_repository_adapter import (
+    ClassRepositoryAdapterDep,
+)
 from server.models.http.requests.class_request_models import ClassRegister, ClassUpdate
 from server.models.http.responses.class_response_models import ClassResponse
 from server.models.http.responses.generic_responses import NoContent
-from server.repositories.class_repository import ClassRepository
 
 embed = Body(..., embed=True)
 
@@ -13,50 +14,52 @@ router = APIRouter(prefix="/classes", tags=["Classes"])
 
 
 @router.get("")
-async def get_all_classes(session: SessionDep) -> list[ClassResponse]:
+async def get_all_classes(repository: ClassRepositoryAdapterDep) -> list[ClassResponse]:
     """Get all classes"""
-    classes = ClassRepository.get_all(session=session)
+    classes = repository.get_all()
     return ClassResponse.from_class_list(classes)
 
 
 @router.get("/{class_id}")
-async def get_class(class_id: int, session: SessionDep) -> ClassResponse:
+async def get_class(
+    class_id: int, repository: ClassRepositoryAdapterDep
+) -> ClassResponse:
     """Get a class by id"""
-    class_ = ClassRepository.get_by_id(id=class_id, session=session)
+    class_ = repository.get_by_id(id=class_id)
     return ClassResponse.from_class(class_)
 
 
 @router.post("")
 async def create_class(
-    class_input: ClassRegister, session: SessionDep
+    class_input: ClassRegister, repository: ClassRepositoryAdapterDep
 ) -> ClassResponse:
     """Create a class"""
-    class_ = ClassRepository.create(input=class_input, session=session)
+    class_ = repository.create(input=class_input)
     return ClassResponse.from_class(class_)
 
 
 @router.put("/{class_id}")
 async def update_class(
-    class_id: int, class_input: ClassUpdate, session: SessionDep
+    class_id: int, class_input: ClassUpdate, repository: ClassRepositoryAdapterDep
 ) -> ClassResponse:
     """Update a class by id"""
-    updated_class = ClassRepository.update(
-        id=class_id, input=class_input, session=session
-    )
+    updated_class = repository.update(id=class_id, input=class_input)
     return ClassResponse.from_class(updated_class)
 
 
 @router.delete("/{class_id}")
-async def delete_class(class_id: int, session: SessionDep) -> Response:
+async def delete_class(
+    class_id: int, repository: ClassRepositoryAdapterDep
+) -> Response:
     """Delete a class by id"""
-    ClassRepository.delete(id=class_id, session=session)
+    repository.delete(id=class_id)
     return NoContent
 
 
 @router.delete("/many/")
 async def delete_many_class(
-    session: SessionDep, ids: Annotated[list[int], Query()] = []
+    repository: ClassRepositoryAdapterDep, ids: Annotated[list[int], Query()] = []
 ) -> Response:
     """Delete a class by id"""
-    ClassRepository.delete_many(ids=ids, session=session)
+    repository.delete_many(ids=ids)
     return NoContent
