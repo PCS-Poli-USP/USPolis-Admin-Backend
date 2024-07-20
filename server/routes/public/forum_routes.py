a6bed6d78acc20390899f0d8b264bc087e5d02d2
danyama <85621221+danyama@users.noreply.github.com>
2024-07-20 17:14:12 -0300

    Resolve PR

--- server/routes/public/forum_routes.py
+++ server/routes/public/forum_routes.py
@@ -9,13 +9,6 @@ from server.models.http.responses.forum_post_response import (
 )
 from server.repositories.forum_repository import ForumRepository
 
-
-import logging
-import sys
-
-logger = logging.getLogger('uvicorn.error')
-logger.setLevel(logging.DEBUG)
-
 embed = Body(..., embed=True)
 
 router = APIRouter(prefix="/forum", tags=["Forum"])
@@ -23,7 +16,6 @@ router = APIRouter(prefix="/forum", tags=["Forum"])
 @router.get("/{class_id}")
 async def get_posts(class_id: int, session: SessionDep) -> list[ForumPostResponse]:
     """Get all posts"""
-    logger.debug('this is a debug message')
     posts = ForumRepository.get_all_posts(class_id=class_id, session=session)
     return ForumPostResponse.from_forum_post_list(posts)
 
@@ -36,8 +28,3 @@ async def create_forum_post(class_id: int, input: ForumPostRegister, session: Se
     ) 
     return ForumPostResponse.from_forum_post(forum_post)
 
-@router.post("")
-async def foo():
-    """Get all posts"""
-    
-    return {'Hello':' World'} 
