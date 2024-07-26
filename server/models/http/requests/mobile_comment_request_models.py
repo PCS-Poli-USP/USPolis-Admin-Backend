fbe12338cfc8b02c3b3df6625f6a385b044b0e0a
VitorMakiyama <jv.makiyama@usp.br>
2024-07-26 17:06:21 -0300

    Added send email when mobile comment is sent

--- server/models/http/requests/mobile_comment_request_models.py
+++ server/models/http/requests/mobile_comment_request_models.py
@@ -5,10 +5,12 @@ from server.models.database.mobile_comments_db_model import Comment
 
 class MobileCommentRegister(BaseModel):
     comment: str
-    email: str | None
+    email: str | None = None
+    created_by: int | None = None
 
 def to_comment_model(c: MobileCommentRegister) -> Comment:
     return Comment(
         comment=c.comment,
-        email=c.email
+        email=c.email,
+        created_by_id=c.created_by
     )
