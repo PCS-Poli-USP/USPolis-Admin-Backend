c00c147c0a71c3a934321024d4b1bacabf5975dc
FastShowDev <zlFastShow@hotmail.com>
2024-09-03 01:04:10 -0300

    Create subject routes tests

--- tests/utils/subject_test_utils.py
+++ tests/utils/subject_test_utils.py
@@ -1,13 +1,16 @@
-# type: ignore
-
+from sqlmodel import Session, select, col
+from server.models.database.building_db_model import Building
 from server.models.database.subject_db_model import Subject
-from server.models.http.requests.subject_request_models import SubjectRegister
-from server.routes.restricted.subject_routes import SubjectCodeAlreadyExists
+from server.models.http.requests.subject_request_models import (
+    SubjectRegister,
+    SubjectUpdate,
+)
 from tests.utils.default_values.test_subject_default_values import SubjectDefaultValues
 
 
-def make_subject(code: str) -> Subject:
+def make_subject(code: str, buildings: list[Building]) -> Subject:
     subject = Subject(
+        buildings=buildings,
         code=code,
         name=SubjectDefaultValues.NAME,
         professors=SubjectDefaultValues.PROFESSORS,
@@ -19,10 +22,11 @@ def make_subject(code: str) -> Subject:
     return subject
 
 
-def make_subject_register_input() -> SubjectRegister:
+def make_subject_register_input(building_ids: list[int]) -> SubjectRegister:
     register = SubjectRegister(
         code=SubjectDefaultValues.CODE,
         name=SubjectDefaultValues.NAME,
+        building_ids=building_ids,
         professors=SubjectDefaultValues.PROFESSORS,
         type=SubjectDefaultValues.TYPE,
         class_credit=SubjectDefaultValues.CLASS_CREDIT,
@@ -32,9 +36,29 @@ def make_subject_register_input() -> SubjectRegister:
     return register
 
 
-async def add_subject(code: str) -> str:
-    if await Subject.check_code_exists(code):
-        raise SubjectCodeAlreadyExists(code)
-    subject = make_subject(code)
-    await subject.create()
-    return str(subject.id)
+def make_subject_update_input(building_ids: list[int]) -> SubjectUpdate:
+    register = SubjectUpdate(
+        code=SubjectDefaultValues.CODE,
+        name=SubjectDefaultValues.NAME,
+        building_ids=building_ids,
+        professors=SubjectDefaultValues.PROFESSORS,
+        type=SubjectDefaultValues.TYPE,
+        class_credit=SubjectDefaultValues.CLASS_CREDIT,
+        work_credit=SubjectDefaultValues.WORK_CREDIT,
+        activation=SubjectDefaultValues.ACTIVATION,
+    )
+    return register
+
+
+def create_subject(session: Session, code: str, buildings: list[Building]) -> int:
+    """Create a default subject with code and buildings"""
+    subject = make_subject(code, buildings)
+    session.add(subject)
+    session.commit()
+    return subject.id  # type: ignore
+
+
+def check_code_exists(db: Session, code: str) -> bool:
+    statement = select(Subject).where(col(Subject.code) == code)
+    result = db.exec(statement).first()
+    return result is not None
