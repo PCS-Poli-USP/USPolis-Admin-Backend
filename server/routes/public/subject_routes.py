dcfb08ff3f02c5fa566a4f6314aefd9c3e3ba7e3
gdvcamargo <zlFastShow@hotmail.com>
2024-11-27 19:26:30 -0300

    Make subjects get all public

---
+++ server/routes/public/subject_routes.py
@@ -0,0 +1,18 @@
+
+from fastapi import APIRouter, Body
+
+from server.models.http.responses.subject_response_models import SubjectResponse
+
+from server.deps.session_dep import SessionDep
+from server.repositories.subject_repository import SubjectRepository
+
+embed = Body(..., embed=True)
+
+router = APIRouter(prefix="/subjects", tags=["Subjects", "Public"])
+
+
+@router.get("")
+async def get_all_subjects(session: SessionDep) -> list[SubjectResponse]:
+    """Get all subjects"""
+    subjects = SubjectRepository.get_all(session=session)
+    return SubjectResponse.from_subject_list(subjects)
