dfbcffc37ec1923d89b65e56fe1c6a6ccf0d1a57
gdvcamargo <zlFastShow@hotmail.com>
2024-12-19 21:05:26 -0300

    Update dump script

--- server/scripts/dumps/load_csv.py
+++ server/scripts/dumps/load_csv.py
@@ -31,53 +31,61 @@ from server.models.database import (  # noqa
 # Cria o engine de conexão com o banco de dados
 engine = create_engine(f"{CONFIG.db_uri}/{CONFIG.db_database}")
 
+
 # Função para ler o CSV e salvar no banco de dados
 def load_csv_to_db(csv_file: str) -> None:
     try:
-        with open(csv_file, mode='r', encoding='utf-8') as file:
+        with open(csv_file, encoding="utf-8") as file:
             # Lê os dados do CSV
             reader = csv.DictReader(file)  # Leitor com cabeçalho do CSV
-            
+
             with Session(engine) as session:
                 for row in reader:
-                    # Cria a instância da entidade Building
-                    building = building_db_model.Building(
-                        id=row['id'], # type: ignore
-                        name=row['name'],
-                        created_by_id=row['created_by_id'], # type: ignore
-                        updated_at=row['updated_at'] # type: ignore
-                    )
-                    
-                    # Adiciona o Building ao banco de dados
-                    session.add(building)
-                    
+                    # # Cria a instância da entidade Building
+                    # building = building_db_model.Building(
+                    #     id=row['id'], # type: ignore
+                    #     name=row['name'],
+                    #     created_by_id=1, # type: ignore
+                    #     updated_at=row['updated_at'] # type: ignore
+                    # )
+
+                    # # Adiciona o Building ao banco de dados
+                    # session.add(building)
+
                     # Caso você tenha uma tabela Classroom também no CSV
-                    if 'classroom_id' in row:
+                    if True:
                         classroom = classroom_db_model.Classroom(
-                            name=row['classroom_name'], # type: ignore
-                            capacity=row['capacity'], # type: ignore
-                            floor=row['floor'], # type: ignore
-                            ignore_to_allocate=row['ignore_to_allocate'], # type: ignore
-                            accessibility=row['accessibility'], # type: ignore
-                            projector=row['projector'], # type: ignore
-                            air_conditioning=row['air_conditioning'], # type: ignore
-                            updated_at=row['updated_at'], # type: ignore
-                            created_by_id=3, # type: ignore
-                            building_id=row['building_id'], # type: ignore
-                            id=row['id'], # type: ignore
+                            name=row["name"],  # type: ignore
+                            capacity=row["capacity"],  # type: ignore
+                            floor=row["floor"],  # type: ignore
+                            ignore_to_allocate=True
+                            if row["ignore_to_allocate"] == "t"
+                            else False,  # type: ignore
+                            accessibility=True
+                            if row["accessibility"] == "t"
+                            else False,  # type: ignore
+                            projector=True if row["projector"] == "t" else False,  # type: ignore
+                            air_conditioning=True
+                            if row["air_conditioning"] == "t"
+                            else False,  # type: ignore
+                            updated_at=row["updated_at"],  # type: ignore
+                            created_by_id=1,  # type: ignore
+                            building_id=row["building_id"],  # type: ignore
+                            id=row["id"],  # type: ignore
                         )
                         session.add(classroom)
-                
+
                 # Commit para salvar todas as instâncias no banco
                 session.commit()
-                
+
         print("Dados carregados com sucesso no banco de dados!")
-    
+
     except Exception as e:
         print(f"Erro ao carregar os dados do CSV: {e}")
 
+
 # Caminho para o arquivo CSV
-csv_file = 'buildings.csv'
+csv_file = "classroom.csv"
 
 # Chama a função para carregar os dados
 load_csv_to_db(csv_file)
