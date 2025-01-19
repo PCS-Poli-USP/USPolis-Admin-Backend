8021e62883385cbcaff70fdfde90c1aff86454e2
gdvcamargo <zlFastShow@hotmail.com>
2025-01-19 14:47:24 -0300

    Create allocation resources route

--- server/routes/public/allocation_routes.py
+++ server/routes/public/allocation_routes.py
@@ -2,7 +2,11 @@ from datetime import date
 from fastapi import APIRouter
 
 from server.deps.session_dep import SessionDep
-from server.models.http.responses.allocation_response_models import EventResponse
+from server.models.http.responses.allocation_response_models import (
+    EventResponse,
+    ResourceResponse,
+)
+from server.repositories.building_repository import BuildingRepository
 from server.repositories.occurrence_repository import OccurrenceRepository
 from server.repositories.schedule_repository import ScheduleRepository
 
@@ -22,3 +26,12 @@ def get_all_allocation_events(
     events = EventResponse.from_occurrence_list(occurrences)
     events.extend(EventResponse.from_schedule_list(schedules))
     return events
+
+
+@router.get("/resources")
+def get_all_allocation_resources(session: SessionDep) -> list[ResourceResponse]:
+    buildings = BuildingRepository.get_all(session=session)
+    resources = ResourceResponse.from_building_list(buildings)
+    resources.append(ResourceResponse.unnallocated_building())
+    resources.append(ResourceResponse.unnallocated_classroom())
+    return resources
