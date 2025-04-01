5f11a8243904047af33f1fcfaeff7df5f4590e94
gdvcamargo <zlFastShow@hotmail.com>
2025-04-01 16:29:15 -0300

    Create janus crawler

---
+++ server/services/janus_crawler/models.py
@@ -0,0 +1,17 @@
+from datetime import date, time
+from pydantic import BaseModel
+from server.utils.enums.week_day import WeekDay
+
+class SubjectInfo(BaseModel):
+    class_code: str
+    subject_name: str
+    subject_code: str
+    total_students: int
+    credits: int
+    start_date: date
+    end_date: date
+    professors: list[str]
+
+    start_time: time | None
+    end_time: time | None
+    week_day: WeekDay | None
