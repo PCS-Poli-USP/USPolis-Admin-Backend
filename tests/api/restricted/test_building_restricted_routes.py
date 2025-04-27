1b85cc8a77681b9c1d05a4e40a60c9d8f72b855d
gdvcamargo <zlFastShow@hotmail.com>
2025-04-27 17:26:51 -0300

    Create building restricted routes tests

---
+++ tests/api/restricted/test_building_restricted_routes.py
@@ -0,0 +1,39 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+from server.models.database.building_db_model import Building
+
+
+URL_PREFIX = "/buildings"
+
+
+def test_get_building_by_id_with_admin_user(
+    building: Building, client: TestClient
+) -> None:
+    response = client.get(f"{URL_PREFIX}/{building.id}")
+    read = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert read["id"] == building.id
+    assert read["name"] == building.name
+
+
+def test_get_building_by_id_with_restricted_user(
+    building: Building, restricted_client: TestClient
+) -> None:
+    response = restricted_client.get(f"{URL_PREFIX}/{building.id}")
+    read = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert read["id"] == building.id
+    assert read["name"] == building.name
+
+
+def test_get_building_by_id_with_common_user(
+    building: Building, common_client: TestClient
+) -> None:
+    response = common_client.get(f"{URL_PREFIX}/{building.id}")
+    read = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert read["id"] == building.id
+    assert read["name"] == building.name
