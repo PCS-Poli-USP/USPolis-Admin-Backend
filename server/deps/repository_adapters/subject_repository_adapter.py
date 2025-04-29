ec497e4227f5aae8d8b1af5635850fe255352a31
gdvcamargo <zlFastShow@hotmail.com>
2025-04-29 20:45:14 -0300

    Fix subject routes permission checking

--- server/deps/repository_adapters/subject_repository_adapter.py
+++ server/deps/repository_adapters/subject_repository_adapter.py
@@ -6,7 +6,14 @@ from server.deps.authenticate import UserDep
 from server.deps.owned_building_ids import OwnedBuildingIdsDep
 from server.deps.session_dep import SessionDep
 from server.models.database.subject_db_model import Subject
+from server.models.http.requests.subject_request_models import (
+    SubjectRegister,
+    SubjectUpdate,
+)
 from server.repositories.subject_repository import SubjectRepository
+from server.services.security.buildings_permission_checker import (
+    BuildingPermissionChecker,
+)
 from server.services.security.subjects_permission_checker import (
     SubjectPermissionChecker,
 )
@@ -23,6 +30,10 @@ class SubjectRepositoryAdapter:
         self.session = session
         self.user = user
         self.checker = SubjectPermissionChecker(user=user, session=session)
+        self.building_checker = BuildingPermissionChecker(
+            user=user,
+            session=session,
+        )
 
     def get_by_id(self, id: int) -> Subject:
         subject = SubjectRepository.get_by_id(id=id, session=self.session)
@@ -38,6 +49,21 @@ class SubjectRepositoryAdapter:
         subject = SubjectRepository.get_by_code(code=code, session=self.session)
         self.checker.check_permission(object=subject)
         return subject
+
+    def create(self, input: SubjectRegister) -> Subject:
+        self.building_checker.check_permission(input.building_ids)
+        subject = SubjectRepository.create(input=input, session=self.session)
+        return subject
+
+    def update(self, id: int, input: SubjectUpdate) -> Subject:
+        self.checker.check_permission(object=id)
+        subject = SubjectRepository.update(id=id, input=input, session=self.session)
+        return subject
+
+    def delete(self, id: int) -> None:
+        subject = SubjectRepository.get_by_id(id=id, session=self.session)
+        self.checker.check_permission(object=subject)
+        SubjectRepository.delete(id=id, session=self.session)
 
 
 SubjectRepositoryDep = Annotated[SubjectRepositoryAdapter, Depends()]
