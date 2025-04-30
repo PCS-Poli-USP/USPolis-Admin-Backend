67a344dab69bf26c2dba0258a0aa635ca52651b4
gdvcamargo <zlFastShow@hotmail.com>
2025-04-29 21:43:19 -0300

    Fix security raising 403 insted 401 when acess token is empty or bearer is not found

--- server/deps/authenticate.py
+++ server/deps/authenticate.py
@@ -15,12 +15,14 @@ from server.services.auth.authentication_client import (
 from server.models.http.requests.user_request_models import UserRegister
 from sqlalchemy.exc import NoResultFound
 
-security = HTTPBearer()
+security = HTTPBearer(auto_error=False)
 
 
 def google_authenticate(
     credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)],
 ) -> AuthUserInfo:
+    if credentials is None or not credentials.credentials:
+        raise InvalidToken()
     access_token = credentials.credentials
     return AuthenticationClient.get_user_info(access_token)
 
@@ -50,7 +52,7 @@ def authenticate(
 
 def admin_authenticate(user: Annotated[User, Depends(authenticate)]) -> None:
     if not user.is_admin:
-        raise HTTPException(status.HTTP_403_FORBIDDEN, "User must be admin")
+        raise HTTPException(status.HTTP_403_FORBIDDEN, "Usuário deve ser administrador")
 
 
 # -- permission authentications :
@@ -66,9 +68,19 @@ def building_authenticate(
         return building
     if user.buildings is None or building not in user.buildings:
         raise HTTPException(
-            status.HTTP_403_FORBIDDEN, "User must have access to building"
+            status.HTTP_403_FORBIDDEN,
+            f"Usuário não tem permissão para acessar o prédio {building.name}",
         )
     return building
+
+
+class InvalidToken(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token inválido",
+        )
+        self.headers = {"WWW-Authenticate": "Bearer"}
 
 
 # exports:
