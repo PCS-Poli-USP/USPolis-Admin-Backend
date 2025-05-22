7752284703a6782c945df28c93f8a40100a62b50
gdvcamargo <zlFastShow@hotmail.com>
2025-05-21 21:46:09 -0300

    Fix reservation routes permission and listing

--- server/repositories/reservation_repository.py
+++ server/repositories/reservation_repository.py
@@ -1,11 +1,15 @@
+from typing import Any
 from fastapi import HTTPException, status
+from sqlalchemy import Select
 from sqlalchemy.exc import NoResultFound
 from sqlmodel import col, select, Session
 
+from server.deps.interval_dep import QueryInterval
 from server.models.database.building_db_model import Building
 from server.models.database.classroom_db_model import Classroom
 from server.models.database.classroom_solicitation_db_model import ClassroomSolicitation
 from server.models.database.reservation_db_model import Reservation
+from server.models.database.schedule_db_model import Schedule
 from server.models.database.user_db_model import User
 from server.models.http.requests.classroom_solicitation_request_models import (
     ClassroomSolicitationApprove,
@@ -26,18 +30,44 @@ from server.utils.must_be_int import must_be_int
 
 class ReservationRepository:
     @staticmethod
-    def get_all(*, session: Session) -> list[Reservation]:
+    def __apply_interval_filter(
+        *,
+        statement: Select,
+        interval: QueryInterval,
+    ) -> Any:
+        if interval.today:
+            statement = statement.join(Schedule).where(
+                col(Schedule.end_date) >= interval.today,
+            )
+
+        if interval.start and interval.end:
+            statement = statement.join(Schedule).where(
+                col(Schedule.start_date) >= interval.start,
+                col(Schedule.end_date) <= interval.end,
+            )
+        return statement
+
+    @staticmethod
+    def get_all(*, session: Session, interval: QueryInterval) -> list[Reservation]:
         statement = select(Reservation)
+        statement = ReservationRepository.__apply_interval_filter(
+            statement=statement, interval=interval
+        )
         reservations = session.exec(statement).all()
         return list(reservations)
 
     @staticmethod
     def get_all_on_buildings(
-        *, building_ids: list[int], session: Session
+        *, building_ids: list[int], session: Session, interval: QueryInterval
     ) -> list[Reservation]:
+        statement = select(Reservation)
+        statement = ReservationRepository.__apply_interval_filter(
+            statement=statement, interval=interval
+        )
         statement = (
-            select(Reservation)
-            .join(Classroom, col(Reservation.classroom_id) == col(Classroom.id))
+            statement.join(
+                Classroom, col(Reservation.classroom_id) == col(Classroom.id)
+            )
             .join(Building, col(Classroom.building_id) == col(Building.id))
             .where(col(Building.id).in_(building_ids))
             .distinct()
@@ -46,9 +76,25 @@ class ReservationRepository:
         return reservations
 
     @staticmethod
+    def get_all_on_classrooms(
+        *, classroom_ids: list[int], session: Session, interval: QueryInterval
+    ) -> list[Reservation]:
+        statement = select(Reservation).where(
+            col(Reservation.classroom_id).in_(classroom_ids)
+        )
+        statement = ReservationRepository.__apply_interval_filter(
+            statement=statement, interval=interval
+        )
+        reservations = list(session.exec(statement).all())
+        return reservations
+
+    @staticmethod
     def get_by_id(*, id: int, session: Session) -> Reservation:
         statement = select(Reservation).where(col(Reservation.id) == id)
-        reservation = session.exec(statement).one()
+        try:
+            reservation = session.exec(statement).one()
+        except NoResultFound:
+            raise ReservationNotFound(id)
         return reservation
 
     @staticmethod
@@ -61,6 +107,19 @@ class ReservationRepository:
             .join(Building)
             .where(col(Building.id).in_(building_ids))
             .where(col(Reservation.id) == id)
+        )
+        try:
+            reservation = session.exec(statement).one()
+        except NoResultFound:
+            raise ReservationNotFound(id)
+        return reservation
+
+    @staticmethod
+    def get_by_id_on_classrooms(
+        *, id: int, classroom_ids: list[int], session: Session
+    ) -> Reservation:
+        statement = select(Reservation).where(
+            col(Reservation.id) == id, col(Reservation.classroom_id).in_(classroom_ids)
         )
         try:
             reservation = session.exec(statement).one()
@@ -155,17 +214,17 @@ class ReservationRepository:
         return reservation
 
     @staticmethod
-    def update_on_buildings(
+    def update_on_classrooms(
         *,
         id: int,
-        building_ids: list[int],
+        classroom_ids: list[int],
         input: ReservationUpdate,
         classroom: Classroom,
         user: User,
         session: Session,
     ) -> Reservation:
-        reservation = ReservationRepository.get_by_id_on_buildings(
-            id=id, building_ids=building_ids, session=session
+        reservation = ReservationRepository.get_by_id_on_classrooms(
+            id=id, classroom_ids=classroom_ids, session=session
         )
         reservation.title = input.title
         reservation.type = input.type
--- server/routes/public/reservation_routes.py
+++ server/routes/public/reservation_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Body
 
+from server.deps.interval_dep import QueryIntervalDep
 from server.deps.session_dep import SessionDep
 from server.models.http.responses.reservation_response_models import (
     ReservationResponse,
@@ -16,29 +17,29 @@ router = APIRouter(prefix="/reservations", tags=["Public", "Reservations"])
 
 @router.get("")
 async def get_all_reservations(
-    session: SessionDep,
+    session: SessionDep, interval: QueryIntervalDep
 ) -> list[ReservationResponse]:
     """Get all reservations on user buildings"""
-    reservations = ReservationRepository.get_all(session=session)
+    reservations = ReservationRepository.get_all(session=session, interval=interval)
     return ReservationResponse.from_reservation_list(reservations)
 
 
 @router.get("/full/")
 async def get_all_reservations_full(
-    session: SessionDep,
+    session: SessionDep, interval: QueryIntervalDep
 ) -> list[ReservationFullResponse]:
     """Get all reservations with occurrences on user buildings"""
-    reservations = ReservationRepository.get_all(session=session)
+    reservations = ReservationRepository.get_all(session=session, interval=interval)
     return ReservationFullResponse.from_reservation_list(reservations)
 
 
 @router.get("/building/{building_name}")
 async def get_all_reservations_by_building_name(
-    building_name: str, session: SessionDep
+    building_name: str, session: SessionDep, interval: QueryIntervalDep
 ) -> list[ReservationResponse]:
     """Get all classes by building name"""
     building = BuildingRepository.get_by_name(name=building_name, session=session)
     reservations = ReservationRepository.get_all_on_buildings(
-        building_ids=[must_be_int(building.id)], session=session
+        building_ids=[must_be_int(building.id)], session=session, interval=interval
     )
     return ReservationResponse.from_reservation_list(reservations)
