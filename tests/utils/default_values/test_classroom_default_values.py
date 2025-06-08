8bb705c07832de2a56f1260545742a70bf43a947
gdvcamargo <zlFastShow@hotmail.com>
2025-06-07 22:40:35 -0300

    Remove old test files

--- tests/utils/default_values/test_classroom_default_values.py
+++
@@ -1,8 +0,0 @@
-class ClassroomDefaultValues:
-    NAME: str = "Default name"
-    CAPACITY: int = 10
-    FLOOR: int = 1
-    IGNORE_TO_ALLOCATE: bool = False
-    ACCESSIBILITY: bool = False
-    PROJECTOR: bool = True
-    AIR_CONDITIONING: bool = True
--- tests/utils/default_values/test_holiday_category_default_values.py
+++
@@ -1,2 +0,0 @@
-class HolidayCategoryDefaultValues:
-    NAME: str = "default name"
