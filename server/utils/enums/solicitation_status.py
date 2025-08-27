fa9f1d84cc9e2133a707965f8ce2b0c5060c703f
gdvcamargo <zlFastShow@hotmail.com>
2025-08-27 11:48:23 -0300

    Create status detail function on SolicitationStatus enum

--- server/utils/enums/solicitation_status.py
+++ server/utils/enums/solicitation_status.py
@@ -17,3 +17,14 @@ class SolicitationStatus(str, Enum):
             SolicitationStatus.DENIED,
             SolicitationStatus.DELETED,
         ]
+
+    @staticmethod
+    def get_status_detail(status: "SolicitationStatus") -> str:
+        status_details = {
+            SolicitationStatus.PENDING: "A solicitação está pendente.",
+            SolicitationStatus.APPROVED: "A solicitação foi aprovada.",
+            SolicitationStatus.DENIED: "A solicitação foi negada.",
+            SolicitationStatus.CANCELLED: "A solicitação foi cancelada.",
+            SolicitationStatus.DELETED: "A solicitação foi excluída.",
+        }
+        return status_details.get(status, "Status desconhecido.")
