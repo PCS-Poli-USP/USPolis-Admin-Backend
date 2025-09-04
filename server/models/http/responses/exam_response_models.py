23ba136c8734a4605e626e0262ce0b9dc5f1dff7
gdvcamargo <zlFastShow@hotmail.com>
2025-09-03 21:01:20 -0300

    Create ExamEventResponse

--- server/models/http/responses/exam_response_models.py
+++ server/models/http/responses/exam_response_models.py
@@ -2,8 +2,11 @@ from typing import Self
 from pydantic import BaseModel
 
 from server.models.database.exam_db_model import Exam
+from server.models.database.occurrence_db_model import Occurrence
+from server.models.http.responses.allocation_response_models import RRule
 from server.models.http.responses.class_response_models import ClassResponseBase
 from server.models.http.responses.reservation_response_models import ReservationResponse
+from server.utils.enums.recurrence import Recurrence
 from server.utils.must_be_int import must_be_int
 
 
@@ -32,3 +35,59 @@ class ExamResponse(BaseModel):
     @classmethod
     def from_exams(cls, exams: list[Exam]) -> list[Self]:
         return [cls.from_exam(exam) for exam in exams]
+
+
+class ExamEventExtendedProps(BaseModel):
+    subject_code: str
+
+
+class ExamEventResponse(BaseModel):
+    id: str
+    title: str
+    start: str
+    end: str
+
+    rrule: RRule | None = None
+    resourceId: str | None = None
+    extendedProps: ExamEventExtendedProps | None = None
+
+    @classmethod
+    def from_exam_occurrence(
+        cls, exam: Exam, occurrence: Occurrence
+    ) -> "ExamEventResponse":
+        reservation = exam.reservation
+        return cls(
+            id=str(must_be_int(exam.id)),
+            title=reservation.title,
+            start=f"{occurrence.date}T{occurrence.start_time}",
+            end=f"{occurrence.date}T{occurrence.end_time}",
+            extendedProps=ExamEventExtendedProps(
+                subject_code=exam.subject.code,
+            ),
+        )
+
+    @classmethod
+    def from_exam(cls, exam: Exam) -> list["ExamEventResponse"]:
+        reservation = exam.reservation
+        schedule = reservation.schedule
+        if schedule.recurrence == Recurrence.CUSTOM:
+            return [cls.from_exam_occurrence(exam, o) for o in schedule.occurrences]
+        return [
+            cls(
+                id=str(must_be_int(exam.id)),
+                title=reservation.title,
+                start=f"{schedule.start_date}T{schedule.start_time}",
+                end=f"{schedule.end_date}T{schedule.end_time}",
+                rrule=RRule.from_schedule(reservation.schedule),
+                extendedProps=ExamEventExtendedProps(
+                    subject_code=exam.subject.code,
+                ),
+            )
+        ]
+
+    @classmethod
+    def from_exams(cls, exams: list[Exam]) -> list["ExamEventResponse"]:
+        response = []
+        for exam in exams:
+            response.extend(cls.from_exam(exam))
+        return response
