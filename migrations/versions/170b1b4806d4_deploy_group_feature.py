5d4acb354e199043d199cf7cbbb24d1138e81989
internuvem-uspolis <uspolis@usp.br>
2025-09-28 01:28:50 +0000

    Include all server migrations to migrations folder

--- migrations/versions/170b1b4806d4_deploy_group_feature.py
+++ migrations/versions/170b1b4806d4_deploy_group_feature.py
@@ -1,8 +1,8 @@
-"""Deploy allocation log branch and deleted by in solicitation
+"""Deploy group feature  26-05-2025
 
-Revision ID: 7af3b512d761
-Revises: 3a1eb5622840, 78a13f742e39
-Create Date: 2025-03-23 19:50:50.893528
+Revision ID: 170b1b4806d4
+Revises: 21f1e6abbff1, 98e28ed3304b
+Create Date: 2025-05-26 02:58:39.767209
 
 """
 from typing import Sequence, Union
@@ -13,8 +13,8 @@ import sqlmodel
 
 
 # revision identifiers, used by Alembic.
-revision: str = '7af3b512d761'
-down_revision: Union[str, None] = ('3a1eb5622840', '78a13f742e39')
+revision: str = '170b1b4806d4'
+down_revision: Union[str, None] = ('21f1e6abbff1', '98e28ed3304b')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
--- migrations/versions/b900ec39608c_deploy_remove_group_unique_name.py
+++ migrations/versions/b900ec39608c_deploy_remove_group_unique_name.py
@@ -1,8 +1,8 @@
-"""Deploy Janus Crawler
+"""deploy_remove_group_unique_name
 
-Revision ID: 21f1e6abbff1
-Revises: 7af3b512d761, d41e4f7ca46f
-Create Date: 2025-04-03 05:10:12.009777
+Revision ID: b900ec39608c
+Revises: 170b1b4806d4, f09c938b75d2
+Create Date: 2025-05-28 16:31:12.552640
 
 """
 from typing import Sequence, Union
@@ -13,8 +13,8 @@ import sqlmodel
 
 
 # revision identifiers, used by Alembic.
-revision: str = '21f1e6abbff1'
-down_revision: Union[str, None] = ('7af3b512d761', 'd41e4f7ca46f')
+revision: str = 'b900ec39608c'
+down_revision: Union[str, None] = ('170b1b4806d4', 'f09c938b75d2')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
