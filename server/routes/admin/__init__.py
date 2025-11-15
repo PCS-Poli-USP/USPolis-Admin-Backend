2c9611581654352a1e473684e0e96a5f7da7228f
gdvcamargo <zlFastShow@hotmail.com>
2025-11-15 19:34:19 -0300

    [feature]: create admin cookie routes for bug reports evidences

--- server/routes/admin/__init__.py
+++ server/routes/admin/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from server.deps.authenticate import admin_authenticate
+from server.deps.authenticate import admin_authenticate, admin_authenticate_from_cookie
 from server.routes.admin.building_admin_routes import router as AdminBuildingRouter
 from server.routes.admin.user_admin_routes import router as AdminUserRouter
 from server.routes.admin.mobile_admin_routes import router as AdminMobileRouter
@@ -8,6 +8,7 @@ from server.routes.admin.group_admin_routes import router as AdminGroupRouter
 from server.routes.admin.feedback_admin_routes import router as AdminFeedbackRouter
 from server.routes.admin.bug_report_admin_routes import router as AdminBugReportRouter
 from server.routes.admin.bug_report_evidence_admin_routes import (
+    cookie_router as AdminBugReportEvidenceCookieRouter,
     router as AdminBugReportEvidenceRouter,
 )
 
@@ -22,3 +23,11 @@ router.include_router(AdminGroupRouter)
 router.include_router(AdminFeedbackRouter)
 router.include_router(AdminBugReportRouter)
 router.include_router(AdminBugReportEvidenceRouter)
+
+cookie_router = APIRouter(
+    prefix="/images/admin",
+    tags=["Admin", "Cookie"],
+    dependencies=[Depends(admin_authenticate_from_cookie)],
+)
+
+cookie_router.include_router(AdminBugReportEvidenceCookieRouter)
