dc07237934e1bee2ff16d13213f8e1bbc7548b11
Julia <juliamboschetti@gmail.com>
2026-02-02 22:23:32 -0300

    [bugfix]: improvements in SQLModel for courses

--- server/models/database/course_db_model.py
+++ server/models/database/course_db_model.py
@@ -1,8 +1,15 @@
-from sqlmodel import Field
+from typing import TYPE_CHECKING
+
+from sqlmodel import Field, Enum, Relationship
+from sqlalchemy import Column
 from datetime import datetime
 from server.models.database.base_db_model import BaseModel
 from server.utils.brazil_datetime import BrazilDatetime
 from server.utils.enums.course_period_type_enum import CoursePeriodType
+
+if TYPE_CHECKING:
+    from server.models.database.curriculum_db_model import Curriculum
+    from server.models.database.emphasis_db_model import Emphasis
 
 class Course(BaseModel, table=True):
     name: str = Field(unique=True)
@@ -13,4 +20,12 @@ class Course(BaseModel, table=True):
     updated_by_id: int = Field(foreign_key="user.id")
     created_at: datetime = Field(default_factory=BrazilDatetime.now_utc)
     created_by_id: int = Field(foreign_key="user.id")
-    period: "CoursePeriodType" = Field()
+    period: CoursePeriodType = Field(
+        sa_column=Column(Enum(CoursePeriodType), nullable=False),
+    )
+    curriculums: list["Curriculum"] = Relationship(
+        back_populates="course", sa_relationship_kwargs={"cascade": "all, delete"}
+    )
+    emphases: list["Emphasis"] = Relationship(
+        back_populates="course", sa_relationship_kwargs={"cascade": "all, delete"}
+        )
--- server/models/database/curriculum_db_model.py
+++ server/models/database/curriculum_db_model.py
@@ -1,19 +1,17 @@
-from sqlmodel import Field
-from sqlalchemy import Column, ForeignKey, Integer
+from typing import TYPE_CHECKING
+
+from sqlmodel import Field, Relationship
 from server.utils.brazil_datetime import BrazilDatetime
 from server.models.database.base_db_model import BaseModel
 from datetime import datetime
 
+if TYPE_CHECKING:
+    from server.models.database.course_db_model import Course
+    from server.models.database.curriculum_subject_db_model import CurriculumSubject
+
 class Curriculum(BaseModel, table=True):
-    course_id: int = Field(
-        sa_column=Column(Integer, ForeignKey("course.id", ondelete="CASCADE"),
-        nullable=False,)
-    )
-    emphasis_id: int | None = Field(
-        sa_column=Column(Integer, ForeignKey("emphasis.id", ondelete="SET NULL"),
-        nullable=True,),
-        default=None
-    )
+    course_id: int = Field(foreign_key="course.id")
+    emphasis_id: int | None = Field(foreign_key="emphasis.id", default=None)
     required_class_hours: int = Field()
     required_work_hours: int = Field()
     optional_free_class_hours: int = Field()
@@ -26,3 +24,7 @@ class Curriculum(BaseModel, table=True):
     updated_by_id: int = Field(foreign_key="user.id")
     created_at: datetime = Field(default_factory=BrazilDatetime.now_utc)
     created_by_id: int = Field(foreign_key="user.id")
+    course: "Course" = Relationship(back_populates="curriculums")
+    subjects: list["CurriculumSubject"] = Relationship(
+        back_populates="curriculum", sa_relationship_kwargs={"cascade": "all, delete"}
+        )
