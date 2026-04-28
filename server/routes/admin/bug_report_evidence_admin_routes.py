102cdaf88366a11006e16cf2f6705fdcc2a0961f
gdvcamargo <zlFastShow@hotmail.com>
2026-04-28 18:59:00 -0300

    [bugfix]: auth routes, unified at once auth, dont duplicate routes anymore, uses token or cookies to auth users, create a hierarchy auth

--- server/routes/admin/bug_report_evidence_admin_routes.py
+++ server/routes/admin/bug_report_evidence_admin_routes.py
@@ -1,4 +1,3 @@
-from asyncio import sleep
 from fastapi import APIRouter, Body, Response
 
 from server.deps.session_dep import SessionDep
@@ -8,18 +7,6 @@ from server.repositories.bug_report_evidence_repository import (
 
 embed = Body(..., embed=True)
 
-cookie_router = APIRouter(
-    prefix="/reports/evidences",
-    tags=["BugReports", "BugReportEvidences"],
-)
-
-
-@cookie_router.get("/{evidence_id}")
-def get_report_evidence_cookie(evidence_id: int, session: SessionDep) -> Response:
-    evidence = BugReportEvidenceRepository.get_by_id(id=evidence_id, session=session)
-    return Response(content=evidence.data, media_type=evidence.mime_type)
-
-
 router = APIRouter(
     prefix="/reports/evidences",
     tags=["BugReports", "BugReportEvidences"],
@@ -27,7 +14,6 @@ router = APIRouter(
 
 
 @router.get("/{evidence_id}")
-async def get_report_evidence(evidence_id: int, session: SessionDep) -> Response:
+def get_report_evidence(evidence_id: int, session: SessionDep) -> Response:
     evidence = BugReportEvidenceRepository.get_by_id(id=evidence_id, session=session)
-    await sleep(3)
     return Response(content=evidence.data, media_type=evidence.mime_type)
