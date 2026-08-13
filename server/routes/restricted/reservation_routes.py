3bbc840289b3be7cb8290bb112a3c9be71b4371f
Julia <juliamboschetti@gmail.com>
2026-08-13 18:28:33 -0300

    [improvement]: Update occurrences for reservations

--- server/routes/restricted/reservation_routes.py
+++ server/routes/restricted/reservation_routes.py
@@ -2,11 +2,23 @@ import asyncio
 from fastapi import APIRouter, Body
 from fastapi.responses import JSONResponse
 
+from server.deps.authenticate import UserDep
 from server.deps.repository_adapters.reservation_repository_adapter import (
     ReservationRepositoryDep,
 )
-from server.models.http.responses.reservation_response_models import ReservationResponse
+from server.deps.session_dep import SessionDep
+from server.models.http.requests.schedule_request_models import (
+    ScheduleUpdateOccurrences,
+)
+from server.models.http.responses.reservation_response_models import (
+    ReservationFullResponse,
+    ReservationResponse,
+)
 from server.services.email.email_service import EmailService
+from server.services.security.reservation_permission_checker import (
+    ReservationPermissionChecker,
+)
+from server.repositories.reservation_repository import ReservationRepository
 
 from server.routes.restricted.exam_routes import router as ExamRouter
 from server.routes.restricted.event_routes import router as EventRouter
@@ -20,6 +32,13 @@ router.include_router(ExamRouter)
 router.include_router(EventRouter)
 router.include_router(MeetingRouter)
 
+@router.get("/{reservation_id}/full", response_model=ReservationFullResponse)
+def get_reservation_full(
+    reservation_id: int,
+    repository: ReservationRepositoryDep,
+) -> ReservationFullResponse:
+    reservation = repository.get_by_id(id=reservation_id)
+    return ReservationFullResponse.from_reservation(reservation)
 
 @router.get("/{reservation_id}", response_model=ReservationResponse)
 def get_reservation(
@@ -28,6 +47,21 @@ def get_reservation(
 ) -> ReservationResponse:
     reservation = repository.get_by_id(id=reservation_id)
     return ReservationResponse.from_reservation(reservation)
+
+@router.patch("/{reservation_id}/edit-occurrences")
+def update_reservation_occurrences(
+    reservation_id: int,
+    input: ScheduleUpdateOccurrences,
+    user: UserDep,
+    session: SessionDep,
+) -> ReservationFullResponse:
+    checker = ReservationPermissionChecker(user=user, session=session)
+    checker.check_permission(reservation_id)
+    reservation = ReservationRepository.update_occurrences(
+        id=reservation_id, input=input, session=session
+    )
+    session.commit()
+    return ReservationFullResponse.from_reservation(reservation)
 
 @router.delete("/{reservation_id}")
 async def delete_reservation(
